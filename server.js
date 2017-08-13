42c827d1bbbdff7d148e78f2aad4db13d4129694
naziabilal <naziabilal15@gmail.com>
2017-08-13 13:43:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/naziabilal15', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'naziabilal15'));
 });
 
