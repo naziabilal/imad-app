ab443f68fd027a54084af96d6cbd28d6ac1bdcd1
naziabilal <naziabilal15@gmail.com>
2017-08-13 14:33:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/naaaz', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'naaz.html'));
+ res.send('naaz is requesting ');
 });
 
 app.get('/ui/style.css', function (req, res) {
