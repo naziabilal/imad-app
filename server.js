e9f1921c1f496568393b76b75fe259db36a0e4a3
naziabilal <naziabilal15@gmail.com>
2017-08-13 14:55:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/iqra', function (req, res) {
-  res.send('im requesting nd contents are');
+  res.sendFile(path.join(__dirname, 'ui', 'iqra.html')); 
 });
 
 
